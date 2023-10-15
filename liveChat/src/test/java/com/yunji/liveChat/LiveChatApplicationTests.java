589ba82bc852ced755b40db2ca66106ac815ff39
yunjino <1tutelary@gmail.com>
2023-10-15 19:49:57 +0900

    first commit

---
+++ liveChat/src/test/java/com/yunji/liveChat/LiveChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunji.liveChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
